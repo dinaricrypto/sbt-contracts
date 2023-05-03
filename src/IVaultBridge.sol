92e8477cc10186389050c0ed73d971863950dc12
Jake <jake.a.timothy@gmail.com>
2023-05-03 09:31:18 -0700

    cancelOrder (#19)

--- src/IVaultBridge.sol
+++ src/IVaultBridge.sol
@@ -32,6 +32,8 @@ interface IVaultBridge {
     event OrderRequested(bytes32 indexed id, address indexed user, Order order);
     event OrderFill(bytes32 indexed id, address indexed user, uint256 fillAmount);
     event OrderFulfilled(bytes32 indexed id, address indexed user, uint256 filledAmount);
+    event CancelRequested(bytes32 indexed id, address indexed user);
+    event OrderCancelled(bytes32 indexed id, address indexed user, string reason);
 
     function getOrderId(Order calldata order, bytes32 salt) external view returns (bytes32);
 
@@ -43,4 +45,8 @@ interface IVaultBridge {
 
     function fillOrder(Order calldata order, bytes32 salt, uint256 assetTokenQuantity, uint256 paymentTokenQuantity)
         external;
+
+    function requestCancel(Order calldata order, bytes32 salt) external;
+
+    function cancelOrder(Order calldata order, bytes32 salt, string calldata reason) external;
 }
