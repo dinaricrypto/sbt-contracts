ae250af8f50f180837cd79dd4ea9c699d53749f3
jaketimothy <jake.a.timothy@gmail.com>
2023-05-03 09:28:46 -0700

    token factory

---
+++ src/BridgedTokenFactory.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "./BridgedERC20.sol";
+
+/// @notice .
+/// @author Dinari (https://github.com/dinaricrypto/issuer-contracts/blob/main/src/BridgedTokenFactory.sol)
+contract BridgedTokenFactory {
+    event TokenDeployed(address indexed tokenAddress);
+
+    function deployBridgedERC20(
+        address owner,
+        string memory name,
+        string memory symbol,
+        string memory disclosures,
+        ITransferRestrictor transferRestrictor
+    ) external returns (BridgedERC20 token) {
+        token = new BridgedERC20(owner, name, symbol, disclosures, transferRestrictor);
+        emit TokenDeployed(address(token));
+    }
+}
