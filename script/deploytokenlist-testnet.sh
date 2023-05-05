b7bfe26ec8bc2f79fd01e4c95436311f08b1618c
Jake <jake.a.timothy@gmail.com>
2023-05-05 10:51:06 -0700

    deploy token list (#28)

---
+++ script/deploytokenlist-testnet.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+source .env
+export TOKEN_NAME="$1"
+export TOKEN_SYMBOL="$2"
+
+forge script script/DeployTokenList.s.sol:DeployTokenListScript --rpc-url $ARBITRUM_GOERLI_RPC_URL --broadcast --verify -vvvv
