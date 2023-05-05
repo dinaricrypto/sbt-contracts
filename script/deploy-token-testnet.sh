3232a7818fee0cf89f92a8663107a18e78d3d940
jaketimothy <jake.a.timothy@gmail.com>
2023-05-05 10:48:35 -0700

    deploy new testnet

--- script/deploy-token-testnet.sh
+++ script/deploy-token-testnet.sh
@@ -4,4 +4,4 @@ source .env
 export TOKEN_NAME="$1"
 export TOKEN_SYMBOL="$2"
 
-forge script script/DeployToken.s.sol:DeployTokenScript --rpc-url $ARBITRUM_GOERLI_RPC_URL --broadcast --verify -vvvv
+forge script script/DeployToken.s.sol:DeployTokenScript --rpc-url $TEST_RPC_URL --broadcast --verify -vvvv
