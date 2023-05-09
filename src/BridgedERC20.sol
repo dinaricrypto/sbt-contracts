d75e711ca9111b8eef8fb9d7c39b109c9fa81500
jaketimothy <jake.a.timothy@gmail.com>
2023-05-09 00:13:03 -0700

    remove multicallable

--- src/BridgedERC20.sol
+++ src/BridgedERC20.sol
@@ -2,13 +2,12 @@
 pragma solidity ^0.8.18;
 
 import "solady/tokens/ERC20.sol";
-import "solady/utils/Multicallable.sol";
 import "solady/auth/OwnableRoles.sol";
 import "./ITransferRestrictor.sol";
 
 /// @notice ERC20 with minter and blacklist.
 /// @author Dinari (https://github.com/dinaricrypto/issuer-contracts/blob/main/src/BridgedERC20.sol)
-contract BridgedERC20 is ERC20, Multicallable, OwnableRoles {
+contract BridgedERC20 is ERC20, OwnableRoles {
     // TODO: compare with openeden vault
     event NameSet(string name);
     event SymbolSet(string symbol);
