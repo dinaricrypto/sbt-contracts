6df4ccd27e226c67a8a6cd42ae196435df418f9b
jaketimothy <jake.a.timothy@gmail.com>
2023-06-01 11:48:53 -0700

    order fees test passing

--- src/OrderFees.sol
+++ src/OrderFees.sol
@@ -10,6 +10,7 @@ import "./IOrderFees.sol";
 /// @author Dinari (https://github.com/dinaricrypto/issuer-contracts/blob/main/src/OrderFees.sol)
 contract OrderFees is Ownable, IOrderFees {
     error FeeTooLarge();
+    error DecimalsTooLarge();
 
     event FeeSet(uint64 perOrderFee, uint64 percentageFee);
 
@@ -41,13 +42,14 @@ contract OrderFees is Ownable, IOrderFees {
 
     /// @inheritdoc IOrderFees
     function feesForOrder(address token, bool, uint256 value) external view returns (uint256 fee) {
-        fee = perOrderFee;
         uint8 decimals = IERC20Metadata(token).decimals();
+        if (decimals > 18) revert DecimalsTooLarge();
+        fee = perOrderFee;
+        // adjust flat fee to token decimals
         if (decimals < 18) {
             fee /= 10 ** (18 - decimals);
-        } else if (decimals > 18) {
-            fee *= 10 ** (decimals - 18);
         }
+        // add percentage fee
         uint64 _percentageFee = percentageFee;
         if (_percentageFee != 0 && value != 0) {
             fee += PrbMath.mulDiv18(value, _percentageFee);
--- test/OrderFees.t.sol
+++ test/OrderFees.t.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "solady-test/utils/mocks/MockERC20.sol";
 import "solady/auth/Ownable.sol";
 import "../src/OrderFees.sol";
 
@@ -9,16 +10,39 @@ contract OrderFeesTest is Test {
     event FeeSet(uint64 perOrderFee, uint64 percentageFee);
 
     OrderFees orderFees;
+    MockERC20 token;
 
     function setUp() public {
         orderFees = new OrderFees(address(this), 1 ether, 0.005 ether);
+        token = new MockERC20("Test Token", "TEST", 18);
     }
 
-    function testInit(address token, bool sell, uint128 value) public {
-        assertEq(orderFees.feesForOrder(token, sell, value), 1 ether + PrbMath.mulDiv18(value, 0.005 ether));
+    function perOrderFeeAdjust(uint8 decimals, uint256 fee) internal pure returns (uint256) {
+        uint256 adjFee = fee;
+        if (decimals < 18) {
+            adjFee /= 10 ** (18 - decimals);
+        } else if (decimals > 18) {
+            adjFee *= 10 ** (decimals - 18);
+        }
+        return adjFee;
     }
 
-    function testSetFee(uint64 perOrderFee, uint64 percentageFee, address token, bool sell, uint128 value) public {
+    function testInit(uint8 tokenDecimals, bool sell, uint128 value) public {
+        MockERC20 newToken = new MockERC20("Test Token", "TEST", tokenDecimals);
+        if (tokenDecimals > 18) {
+            vm.expectRevert(OrderFees.DecimalsTooLarge.selector);
+            orderFees.feesForOrder(address(newToken), sell, value);
+        } else {
+            assertEq(orderFees.perOrderFee(), 1 ether);
+            assertEq(orderFees.percentageFee(), 0.005 ether);
+            assertEq(
+                orderFees.feesForOrder(address(newToken), sell, value),
+                perOrderFeeAdjust(tokenDecimals, 1 ether) + PrbMath.mulDiv18(value, 0.005 ether)
+            );
+        }
+    }
+
+    function testSetFee(uint64 perOrderFee, uint64 percentageFee, bool sell, uint128 value) public {
         if (percentageFee > 1 ether) {
             vm.expectRevert(OrderFees.FeeTooLarge.selector);
             orderFees.setFees(perOrderFee, percentageFee);
@@ -29,7 +53,7 @@ contract OrderFeesTest is Test {
             assertEq(orderFees.perOrderFee(), perOrderFee);
             assertEq(orderFees.percentageFee(), percentageFee);
             assertEq(
-                orderFees.feesForOrder(token, sell, value),
+                orderFees.feesForOrder(address(token), sell, value),
                 uint256(perOrderFee) + PrbMath.mulDiv18(value, percentageFee)
             );
         }
