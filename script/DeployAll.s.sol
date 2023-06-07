22f15e19a88ad2df1944d8ec51e21d5dd5173cb8
jaketimothy <jake.a.timothy@gmail.com>
2023-06-06 22:27:09 -0700

    remove peripheral contracts

--- script/DeployAll.s.sol
+++ script/DeployAll.s.sol
@@ -2,9 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import {Messager} from "../src/Messager.sol";
 import {TransferRestrictor} from "../src/TransferRestrictor.sol";
-import {BridgedTokenFactory} from "../src/BridgedTokenFactory.sol";
 import {OrderFees, IOrderFees} from "../src/issuer/OrderFees.sol";
 import {DirectBuyIssuer} from "../src/issuer/DirectBuyIssuer.sol";
 import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
@@ -19,14 +17,8 @@ contract DeployAllScript is Script {
         // send txs as deployer
         vm.startBroadcast(deployerPrivateKey);
 
-        // deploy messager
-        new Messager();
-
         // deploy transfer restrictor
         new TransferRestrictor(deployer);
-
-        // deploy token factory
-        new BridgedTokenFactory();
 
         // deploy fee manager
         IOrderFees orderFees = new OrderFees(deployer, 1 ether, 0.005 ether);
