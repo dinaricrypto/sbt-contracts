98bab9135dccc3714a02a148e5a88a3ed39789af
Jake <jake.a.timothy@gmail.com>
2023-06-06 22:53:30 -0700

    add buy and sell processors to deployall (#62)

--- script/DeployAll.s.sol
+++ script/DeployAll.s.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.8.13;
 import "forge-std/Script.sol";
 import {TransferRestrictor} from "../src/TransferRestrictor.sol";
 import {OrderFees, IOrderFees} from "../src/issuer/OrderFees.sol";
+import {BuyOrderIssuer} from "../src/issuer/BuyOrderIssuer.sol";
+import {SellOrderProcessor} from "../src/issuer/SellOrderProcessor.sol";
 import {DirectBuyIssuer} from "../src/issuer/DirectBuyIssuer.sol";
 import "openzeppelin-contracts/contracts/proxy/ERC1967/ERC1967Proxy.sol";
 
@@ -23,9 +25,19 @@ contract DeployAllScript is Script {
         // deploy fee manager
         IOrderFees orderFees = new OrderFees(deployer, 1 ether, 0.005 ether);
 
-        // deploy DirectBuyIssuer implementation
+        // deploy implementation
+        BuyOrderIssuer buyImpl = new BuyOrderIssuer();
+        // deploy proxy and set implementation
+        new ERC1967Proxy(address(buyImpl), abi.encodeCall(buyImpl.initialize, (deployer, treasuryAddress, orderFees)));
+
+        // deploy implementation
+        SellOrderProcessor sellImpl = new SellOrderProcessor();
+        // deploy proxy and set implementation
+        new ERC1967Proxy(address(sellImpl), abi.encodeCall(sellImpl.initialize, (deployer, treasuryAddress, orderFees)));
+
+        // deploy implementation
         DirectBuyIssuer directIssuerImpl = new DirectBuyIssuer();
-        // deploy proxy for DirectBuyIssuer and set implementation
+        // deploy proxy and set implementation
         new ERC1967Proxy(address(directIssuerImpl), abi.encodeCall(directIssuerImpl.initialize, (deployer, treasuryAddress, orderFees)));
 
         vm.stopBroadcast();
