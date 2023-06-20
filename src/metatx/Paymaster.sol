341697686c9823f04e5dc6cd8b7667ddb2b63608
joshualyguessennd <joshualyguessennd@icloud.com>
2023-06-20 18:41:22 +0000

    feat: add pretty

--- src/metatx/Paymaster.sol
+++ src/metatx/Paymaster.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.19;
 
 import {BaseRelayRecipient} from "gsnV3/contracts/src/BaseRelayRecipient.sol";
 import "gsnV3/contracts/src/utils/GsnTypes.sol";
-abstract contract Paymaster is BaseRelayRecipient {
 
+abstract contract Paymaster is BaseRelayRecipient {
     /**
      * @notice Mapping to keep track of each user's opt-in status for using the paymaster.
      * @dev Key is user's address, and value is a boolean indicating whether the user has opted in.
@@ -14,7 +14,7 @@ abstract contract Paymaster is BaseRelayRecipient {
     error UserNotOptedIn();
 
     constructor(address forwarder) {
-       _setTrustedForwarder(forwarder);
+        _setTrustedForwarder(forwarder);
     }
 
     /**
@@ -32,7 +32,6 @@ abstract contract Paymaster is BaseRelayRecipient {
         isUserOptedIn[msg.sender] = false;
     }
 
-    
     /**
      * @notice Approves a relayed call if it meets certain conditions.
      * @param relayRequest Data structure that contains information about the relayed call.
@@ -41,38 +40,32 @@ abstract contract Paymaster is BaseRelayRecipient {
      * @param r ECDSA signature parameter r.
      * @param s ECDSA signature parameter s.
      * @return Returns empty bytes if the relayed call is approved, reverts otherwise.
-    */
+     */
     function approveRelayCall(
         GsnTypes.RelayRequest calldata relayRequest,
         uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
-    )
-        external
-        view
-        returns (bytes memory)
-    {
+    ) external view returns (bytes memory) {
         // Check if the transaction has been signed by the user
-        bytes32 domainSeparator = keccak256(abi.encode(
-            keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
-            keccak256(bytes("Paymaster")),
-            keccak256(bytes("1")),
-            block.chainid,
-            address(this)
-        ));
+        bytes32 domainSeparator = keccak256(
+            abi.encode(
+                keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
+                keccak256(bytes("Paymaster")),
+                keccak256(bytes("1")),
+                block.chainid,
+                address(this)
+            )
+        );
 
-        bytes32 structHash = keccak256(abi.encode(
-            keccak256("RelayRequest(GSNTypes.RelayRequest relayRequest,uint256 deadline)"),
-            relayRequest,
-            deadline
-        ));
+        bytes32 structHash = keccak256(
+            abi.encode(
+                keccak256("RelayRequest(GSNTypes.RelayRequest relayRequest,uint256 deadline)"), relayRequest, deadline
+            )
+        );
 
-        bytes32 digest = keccak256(abi.encodePacked(
-            "\x19\x01",
-            domainSeparator,
-            structHash
-        ));
+        bytes32 digest = keccak256(abi.encodePacked("\x19\x01", domainSeparator, structHash));
 
         // Recover the signer's address from the signature
         address signer = ecrecover(digest, v, r, s);
@@ -86,5 +79,4 @@ abstract contract Paymaster is BaseRelayRecipient {
 
         return ""; // No context needed, return empty bytes
     }
-
 }
