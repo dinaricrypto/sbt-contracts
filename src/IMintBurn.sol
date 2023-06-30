9c39a9abad5f4b41111c0d8e538fd4a3cfdc61b8
jaketimothy <jake.a.timothy@gmail.com>
2023-06-30 07:12:15 -0700

    feat: set solc 0.8.19

--- src/IMintBurn.sol
+++ src/IMintBurn.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 /// @notice Interface for token minting and burning
 /// @author Dinari (https://github.com/dinaricrypto/sbt-contracts/blob/main/src/IMintBurn.sol)
--- src/issuer/OrderProcessor.sol
+++ src/issuer/OrderProcessor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity ^0.8.19;
+pragma solidity 0.8.19;
 
 import {Initializable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/Initializable.sol";
 import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol";
