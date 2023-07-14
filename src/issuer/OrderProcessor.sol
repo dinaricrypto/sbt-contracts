1d39d14ce6692182f515df47db875a431723d237
joshualyguessennd <joshualyguessennd@icloud.com>
2023-07-14 23:02:54 +0000

    feat: add cancellation to orderState

--- src/issuer/OrderProcessor.sol
+++ src/issuer/OrderProcessor.sol
@@ -65,6 +65,8 @@ abstract contract OrderProcessor is
         uint256 remainingOrder;
         // Total amount of received token due to fills
         uint256 received;
+        // Whether a cancellation for this order has been initiated
+        bool cancellationInitiated;
     }
 
     /// @dev Zero address
@@ -124,9 +126,6 @@ abstract contract OrderProcessor is
 
     /// @dev Active orders
     mapping(bytes32 => OrderState) private _orders;
-
-    ///@dev Tracks if a cancellation process has been started for a specific order
-    mapping(bytes32 => bool) private orderCancellationInitiated;
 
     /// ------------------ Initialization ------------------ ///
 
@@ -264,7 +263,8 @@ abstract contract OrderProcessor is
 
         // Initialize order state
         uint256 orderAmount = order.sell ? order.assetTokenQuantity : order.paymentTokenQuantity;
-        _orders[orderId] = OrderState({requester: msg.sender, remainingOrder: orderAmount, received: 0});
+        _orders[orderId] =
+            OrderState({requester: msg.sender, remainingOrder: orderAmount, received: 0, cancellationInitiated: false});
         _numOpenOrders++;
     }
 
@@ -318,13 +318,13 @@ abstract contract OrderProcessor is
     function requestCancel(OrderRequest calldata orderRequest, bytes32 salt) external {
         bytes32 orderId = getOrderIdFromOrderRequest(orderRequest, salt);
         address requester = _orders[orderId].requester;
-        if (orderCancellationInitiated[orderId]) revert OrderCancellationAlreadyInitiated();
+        if (_orders[orderId].cancellationInitiated) revert OrderCancellationAlreadyInitiated();
         // Order must exist
         if (requester == address(0)) revert OrderNotFound();
         // Only requester can request cancellation
         if (requester != msg.sender) revert NotRequester();
 
-        orderCancellationInitiated[orderId] = true;
+        _orders[orderId].cancellationInitiated = true;
 
         // Send cancel request to bridge
         emit CancelRequested(orderId, orderRequest.recipient);
