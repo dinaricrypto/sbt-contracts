2354f1292e0cb49894fba2613e6e7a56322045cd
joshualyguessennd <joshualyguessennd@icloud.com>
2023-07-20 20:15:03 +0000

    feat: aggregator interface

---
+++ proof-of-reserve/interfaces/IProofOfReserveAggregator.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.19;
+
+interface IProofOfReserveAggregator {
+    function getProofOfReserveFeedForAsset(address asset) external view returns (address);
+    function enableProofOfReserveFeed(address asset, address feed) external;
+    function disableProofOfReserveFeed(address asset) external;
+    function areReservedBack(address[] calldata asset) external returns (bool, bool[] memory);
+}
