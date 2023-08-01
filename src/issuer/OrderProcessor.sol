75b5d9ce95bb5f9fec7738ba811432dbd70ca92e
joshualyguessennd <joshualyguessennd@icloud.com>
2023-08-01 17:10:51 +0000

    feat: set escrowed balance private

--- src/issuer/OrderProcessor.sol
+++ src/issuer/OrderProcessor.sol
@@ -117,7 +117,7 @@ abstract contract OrderProcessor is
 
     /// @dev Active orders
     mapping(bytes32 => OrderState) private _orders;
-    mapping(address => mapping(address => uint256)) internal escrowedBalance;
+    mapping(address => mapping(address => uint256)) private escrowedBalance;
 
     /// ------------------ Initialization ------------------ ///
 
