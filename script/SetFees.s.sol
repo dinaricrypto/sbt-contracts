11c5758bcf975d5f434b6eeb2fcdbb7e785367af
jaketimothy <jake.a.timothy@gmail.com>
2023-09-22 08:26:17 -0700

    chore: set fees script

---
+++ script/SetFees.s.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "forge-std/Script.sol";
+import {BuyProcessor} from "../src/orders/BuyProcessor.sol";
+// import {SellProcessor} from "../src/orders/SellProcessor.sol";
+import {BuyUnlockedProcessor} from "../src/orders/BuyUnlockedProcessor.sol";
+
+contract SetFeesScript is Script {
+    // When new issuers have been deployed, this script will add tokens to them.
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        BuyProcessor buyIssuer = BuyProcessor(vm.envAddress("BUY_ISSUER"));
+        // SellProcessor sellProcessor = SellProcessor(vm.envAddress("SELL_PROCESSOR"));
+        BuyUnlockedProcessor directIssuer = BuyUnlockedProcessor(vm.envAddress("DIRECT_ISSUER"));
+
+        uint24 percentageFeeRate = 0;
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        buyIssuer.setFees(buyIssuer.perOrderFee(), percentageFeeRate);
+        directIssuer.setFees(directIssuer.perOrderFee(), percentageFeeRate);
+
+        vm.stopBroadcast();
+    }
+}
