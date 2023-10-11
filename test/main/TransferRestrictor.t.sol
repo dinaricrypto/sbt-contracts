7163e24ee4ea2bca137b9b32352fdb628f6ffa54
joshualyguessennd <joshualyguessennd@icloud.com>
2023-10-11 17:22:57 +0000

    feat: fix conflict with main

--- test/main/TransferRestrictor.t.sol
+++ test/main/TransferRestrictor.t.sol
@@ -3,6 +3,7 @@ pragma solidity 0.8.19;
 
 import "forge-std/Test.sol";
 import "../../src/TransferRestrictor.sol";
+import {Strings} from "openzeppelin-contracts/contracts/utils/Strings.sol";
 
 contract TransferRestrictorTest is Test {
     event Restricted(address indexed account);
