b8c9767b1a9bcfd68de19487752bd3039ac502e8
jaketimothy <jake.a.timothy@gmail.com>
2023-10-16 15:26:45 -0700

    chore: move xdshare vault to dividend dir

--- src/dividend/DualDistributor.sol
+++ src/dividend/DualDistributor.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.19;
 import {AccessControlDefaultAdminRules} from
     "openzeppelin-contracts/contracts/access/AccessControlDefaultAdminRules.sol";
 import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {IxdShare} from "../IxdShare.sol";
+import {IxdShare} from "./IxdShare.sol";
 import {IDividendDistributor} from "./IDividendDistributor.sol";
 
 /**
