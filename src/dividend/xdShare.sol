a03e6832d315af90a51e199220fc61e1a8340474
Jake <jake.a.timothy@gmail.com>
2023-10-16 15:35:56 -0700

    chore: move xdshare vault to dividend dir (#221)

--- src/dividend/xdShare.sol
+++ src/dividend/xdShare.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.19;
 
-import {dShare} from "./dShare.sol";
+import {dShare} from "../dShare.sol";
 import {Ownable} from "openzeppelin-contracts/contracts/access/Ownable.sol";
 import {ERC4626, SafeTransferLib} from "solady/src/tokens/ERC4626.sol";
-import {ITransferRestrictor} from "./ITransferRestrictor.sol";
+import {ITransferRestrictor} from "../ITransferRestrictor.sol";
 import {IxdShare} from "./IxdShare.sol";
-import {ITokenManager} from "./ITokenManager.sol";
+import {ITokenManager} from "../ITokenManager.sol";
 import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "openzeppelin-contracts/contracts/security/ReentrancyGuard.sol";
 
--- test/main/DualDistributor.t.sol
+++ test/main/DualDistributor.t.sol
@@ -5,7 +5,7 @@ import "forge-std/Test.sol";
 import {DividendDistribution} from "../../src/dividend/DividendDistribution.sol";
 import {DualDistributor} from "../../src/dividend/DualDistributor.sol";
 import {TransferRestrictor, ITransferRestrictor} from "../../src/TransferRestrictor.sol";
-import {xdShare} from "../../src/xdShare.sol";
+import {xdShare} from "../../src/dividend/xdShare.sol";
 import {dShare} from "../../src/dShare.sol";
 import "solady/test/utils/mocks/MockERC20.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
