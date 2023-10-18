456a4c8aa42fb5302963a5a339dfda73acfb3bfe
Jake <jake.a.timothy@gmail.com>
2023-10-17 21:06:49 -0700

    test: added minter role to mocktoken (#223)
    * test: added minter role to mocktoken
    
    * chore: update mock paymenttoken deploy script

--- test/utils/mocks/MockToken.sol
+++ test/utils/mocks/MockToken.sol
@@ -1,16 +1,44 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity 0.8.19;
 
-import "solady/test/utils/mocks/MockERC20.sol";
+import "solady/src/tokens/ERC20.sol";
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 
-contract MockToken is MockERC20 {
+contract MockToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    string _name;
+    string _symbol;
+
     mapping(address => bool) public isBlacklisted;
     mapping(address => bool) public isBlackListed;
     mapping(address => bool) public isBlocked;
 
-    constructor(string memory name_, string memory symbol_) MockERC20(name_, symbol_, 6) {}
+    constructor(string memory name_, string memory symbol_) {
+        _name = name_;
+        _symbol = symbol_;
 
-    function blacklist(address account) public {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function name() public view override returns (string memory) {
+        return _name;
+    }
+
+    function symbol() public view override returns (string memory) {
+        return _symbol;
+    }
+
+    function decimals() public pure override returns (uint8) {
+        return 6;
+    }
+
+    function mint(address account, uint256 amount) public onlyRole(MINTER_ROLE) {
+        _mint(account, amount);
+    }
+
+    function blacklist(address account) public onlyRole(DEFAULT_ADMIN_ROLE) {
         isBlacklisted[account] = true;
         isBlackListed[account] = true;
         isBlocked[account] = true;
