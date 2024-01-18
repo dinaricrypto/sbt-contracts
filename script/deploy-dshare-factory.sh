a0ad0d9e6d4a92875d8281daeedf48e0726eff7a
jaketimothy <jake.a.timothy@gmail.com>
2024-01-18 09:26:13 -0800

    chore: remove old scripts

--- script/deploy-dshare-factory.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-source .env
-
-forge script script/DeployDShareFactory.s.sol:DeployDshareFactoryScript --rpc-url $RPC_URL --etherscan-api-key $ARBISCAN_API_KEY --broadcast --verify -vvvv
