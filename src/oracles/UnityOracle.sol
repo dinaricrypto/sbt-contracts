88e763b4f4b0033f043f90e4f186fabbb8c0939c
jaketimothy <jake.a.timothy@gmail.com>
2024-02-08 09:45:25 -0800

    feat: unity oracle

---
+++ src/oracles/UnityOracle.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.22;
+
+import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+
+contract UnityOracle is AggregatorV3Interface {
+    function decimals() external pure returns (uint8) {
+        return 8;
+    }
+
+    function description() external pure returns (string memory) {
+        return "One";
+    }
+
+    function version() external pure returns (uint256) {
+        return 1;
+    }
+
+    function getRoundData(uint80)
+        external
+        pure
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        return (0, 1e8, 0, 0, 0);
+    }
+
+    function latestRoundData()
+        external
+        pure
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound)
+    {
+        return (0, 1e8, 0, 0, 0);
+    }
+}
