abfec201dd7e3de0e9485c81836cc9f5c2e03ae0
jaketimothy <jake.a.timothy@gmail.com>
2024-02-14 08:10:20 -0800

    chore: fix setpaymenttoken script

--- script/AddPaymentTokens.s.sol
+++ script/AddPaymentTokens.s.sol
@@ -11,8 +11,6 @@ contract AddPaymentTokens is Script {
     function run() external {
         uint256 deployerPrivateKey = vm.envUint("DEPLOY_KEY");
         OrderProcessor orderProcessor = OrderProcessor(vm.envAddress("ORDERPROCESSOR"));
-        BuyUnlockedProcessor buyUnlockedProcessor = BuyUnlockedProcessor(vm.envAddress("BUYUNLOCKEDPROCESSOR"));
-        Forwarder forwarder = Forwarder(vm.envAddress("FORWARDER"));
 
         address[1] memory paymentTokens = [vm.envAddress("USDPLUS")];
 
@@ -22,12 +20,10 @@ contract AddPaymentTokens is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         for (uint256 i = 0; i < paymentTokens.length; i++) {
-            // add to order processors
-            orderProcessor.setDefaultFees(paymentTokens[i], defaultFees);
-            buyUnlockedProcessor.setDefaultFees(paymentTokens[i], defaultFees);
-
-            // add to forwarder
-            forwarder.setPaymentOracle(paymentTokens[i], paymentTokenOracles[i]);
+            // add payment token
+            orderProcessor.setPaymentTokenOracle(paymentTokens[i], paymentTokenOracles[i]);
+            // set default fees
+            orderProcessor.setFees(address(0), paymentTokens[i], perOrderFee, percentageFeeRate, perOrderFee, percentageFeeRate);
         }
 
         vm.stopBroadcast();
