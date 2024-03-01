48356e28c73d600121410b3d48a62a432bdbb7aa
jaketimothy <jake.a.timothy@gmail.com>
2024-03-01 09:56:33 -0800

    chore: mint script

--- script/transfereth.sh
+++ script/transfereth.sh
@@ -3,4 +3,4 @@
 cp .env.eth-sepolia .env
 source .env
 
-cast send -i 0xc644cc19d2A9388b71dd1dEde07cFFC73237Dca8 --value 0.4ether --rpc-url $RPC_URL
+cast send -i 0xc644cc19d2A9388b71dd1dEde07cFFC73237Dca8 --value 0.5ether --rpc-url $RPC_URL
