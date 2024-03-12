43ba8308763c208711880cac4c62faac1c6e2140
jaketimothy <jake.a.timothy@gmail.com>
2024-03-12 12:52:12 -0700

    chore: reports

--- test/utils/mocks/BlacklistInterfaces.sol
+++ test/utils/mocks/BlacklistInterfaces.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.22;
+pragma solidity ^0.8.22;
 
 // Methods of the form `function blacklist(address account) external returns (bool)`
 
