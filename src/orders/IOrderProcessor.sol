66417ab508aba0d28d547bacb6f9c34934ded66d
jaketimothy <jake.a.timothy@gmail.com>
2024-03-20 21:56:18 -0700

    feat: solc 0.8.25

--- src/orders/IOrderProcessor.sol
+++ src/orders/IOrderProcessor.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 /// @notice Interface for contracts processing orders for dShares
 /// @author Dinari (https://github.com/dinaricrypto/sbt-contracts/blob/main/src/orders/IOrderProcessor.sol)
