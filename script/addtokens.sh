76521bc8ad2e460371c6a4ba372dc3284a5b1f8d
jaketimothy <jake.a.timothy@gmail.com>
2024-07-16 13:56:39 -0700

    chore: sandbox decimal support update

--- script/addtokens.sh
+++ script/addtokens.sh
@@ -3,4 +3,4 @@
 cp .env.sandbox .env
 source .env
 
-forge script script/AddTokens.s.sol:AddTokens --rpc-url $RPC_URL --broadcast -vvvv
+forge script script/AddTokens.s.sol:AddTokens --rpc-url $RPC_URL --broadcast --slow -vvv
