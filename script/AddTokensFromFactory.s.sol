2a8c77b33aae37eb5b113135d5cc30793c427b30
Jake <jake.a.timothy@gmail.com>
2024-08-02 09:46:13 -0700

    chore: check and set dshare permissions (#362)
    * chore: check dshare roles
    
    * chore: log role findings

---
+++ script/AddTokensFromFactory.s.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.22;
+
+import "forge-std/Script.sol";
+import {DShareFactory} from "../src/DShareFactory.sol";
+import {OrderProcessor} from "../src/orders/OrderProcessor.sol";
+import {DShare} from "../src/DShare.sol";
+
+contract AddTokensFromFactory is Script {
+    // When new issuers have been deployed, this script will add tokens to them.
+    function run() external {
+        uint256 deployerPrivateKey = vm.envUint("DEPLOY_KEY");
+        DShareFactory factory = DShareFactory(vm.envAddress("FACTORY"));
+        OrderProcessor issuer = OrderProcessor(vm.envAddress("ORDERPROCESSOR"));
+
+        (address[] memory assetTokens,) = factory.getDShares();
+        bytes32 MINTER_ROLE = DShare(assetTokens[0]).MINTER_ROLE();
+        bytes32 BURNER_ROLE = DShare(assetTokens[0]).BURNER_ROLE();
+
+        vm.startBroadcast(deployerPrivateKey);
+
+        for (uint256 i = 0; i < assetTokens.length; i++) {
+            // issuer.setOrderDecimalReduction(assetTokens[i], DShare(assetTokens[i]).decimals() - 9);
+
+            DShare assetToken = DShare(assetTokens[i]);
+            if (!assetToken.hasRole(MINTER_ROLE, address(issuer))) {
+                console.log("Granting MINTER_ROLE to", address(issuer), "for", assetTokens[i]);
+                assetToken.grantRole(MINTER_ROLE, address(issuer));
+            }
+            if (!assetToken.hasRole(BURNER_ROLE, address(issuer))) {
+                console.log("Granting BURNER_ROLE to", address(issuer), "for", assetTokens[i]);
+                assetToken.grantRole(BURNER_ROLE, address(issuer));
+            }
+        }
+
+        vm.stopBroadcast();
+    }
+}
