2f0f54f5d1389ff8213db20d12827d6da0894068
jaketimothy <jake.a.timothy@gmail.com>
2024-08-15 09:06:12 -0700

    chore: script

--- script/kinto/order-prod.sh
+++ script/kinto/order-prod.sh
@@ -3,4 +3,4 @@
 cp .env.prod-kinto .env
 source .env
 
-forge script script/kinto/CreateOrder.s.sol:CreateOrder --rpc-url $RPC_URL -vvvv # --broadcast --skip-simulation
+forge script script/kinto/CreateOrder.s.sol:CreateOrder --rpc-url $RPC_URL -vvv --broadcast --slow --skip-simulation
