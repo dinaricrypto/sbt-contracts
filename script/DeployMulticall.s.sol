ade4f65fe636d9dacb8f46de19d1f0cc096d65a9
jaketimothy <jake.a.timothy@gmail.com>
2024-08-16 13:09:59 -0700

    feat: add multicall3

--- script/DeployMulticall.s.sol
+++ script/DeployMulticall.s.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Script.sol";
-import "multicall/Multicall3.sol";
+import "../src/common/Multicall3.sol";
 
 contract DeployMulticall is Script {
     function run() external {
