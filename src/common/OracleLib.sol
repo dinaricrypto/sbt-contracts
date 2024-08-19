72b0d7e08431ed8edb6a28514413ea74880bcba9
Jake <jake.a.timothy@gmail.com>
2024-08-19 16:06:06 -0700

    feat: 18 decimals for latest price (#368)

--- src/common/OracleLib.sol
+++ src/common/OracleLib.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.23;
 import "./NumberUtils.sol";
 import {mulDiv} from "prb-math/Common.sol";
 
+/// @dev Assumes asset tokens have 18 decimals.
 library OracleLib {
     function pairIndex(address assetToken, address paymentToken) internal pure returns (bytes32) {
         return keccak256(abi.encodePacked(assetToken, paymentToken));
@@ -14,7 +15,8 @@ library OracleLib {
         pure
         returns (uint256)
     {
-        uint256 decimalMult = 10 ** (18 - paymentTokenDecimals);
+        // Adjusts the payment token decimals to match the asset token decimals (18) and adds 18 decimals to the result.
+        uint256 decimalMult = 10 ** (36 - paymentTokenDecimals);
         if (NumberUtils.mulDivCheckOverflow(paymentTokenQuantity, decimalMult, assetTokenQuantity)) return 0;
         return mulDiv(paymentTokenQuantity, decimalMult, assetTokenQuantity);
     }
