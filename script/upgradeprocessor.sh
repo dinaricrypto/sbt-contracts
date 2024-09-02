f86bdb68362dece27db0494b8b27fc698616dfd6
jaketimothy <jake.a.timothy@gmail.com>
2024-09-02 13:14:21 -0700

    chore: upgrade orderprocessor create2

--- script/upgradeprocessor.sh
+++ script/upgradeprocessor.sh
@@ -1,16 +1,17 @@
 #!/bin/sh
 
+cp .env.staging-sepolia .env
+source .env
+
+forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --verify
+
+cp .env.staging-arb-sepolia .env
+source .env
+
+forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --verify
+
+
 cp .env.sandbox .env
-source .env
-
-forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --verify
-
-cp .env.prod-arb .env
-source .env
-
-forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --verify
-
-cp .env.prod-blast .env
 source .env
 
 forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --verify
@@ -24,3 +25,14 @@ cp .env.prod-eth .env
 source .env
 
 forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --slow --verify
+
+cp .env.prod-kinto .env
+source .env
+
+forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --slow --skip-simulation --verify --verifier blockscout --verifier-url https://explorer.kinto.xyz/api
+
+cp .env.prod-arb .env
+source .env
+
+forge script script/UpgradeProcessor.s.sol:UpgradeProcessor --rpc-url $RPC_URL -vvv --broadcast --verify
+
