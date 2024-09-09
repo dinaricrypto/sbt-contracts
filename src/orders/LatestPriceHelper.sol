042c9a257b37520b59e604ba3f2244159ea97ed2
jaketimothy <jake.a.timothy@gmail.com>
2024-09-09 14:24:19 -0700

    feat: stateless price helper

--- src/orders/LatestPriceHelper.sol
+++ src/orders/LatestPriceHelper.sol
@@ -3,26 +3,16 @@ pragma solidity 0.8.25;
 
 import "./IOrderProcessor.sol";
 
-import "solady/src/auth/Ownable.sol";
-
-contract LatestPriceHelper is Ownable {
-    address[] public paymentTokens;
-
-    function setPaymentTokens(address[] memory _paymentTokens) external onlyOwner {
-        paymentTokens = _paymentTokens;
-    }
-
-    function aggregateLatestPriceFromProcessor(address processor, address token)
+contract LatestPriceHelper {
+    function aggregateLatestPriceFromProcessor(address processor, address token, address[] calldata paymentTokens)
         external
         view
         returns (IOrderProcessor.PricePoint memory latestPricePoint)
     {
-        address[] memory _paymentTokens = paymentTokens;
-
         latestPricePoint = IOrderProcessor.PricePoint({blocktime: 0, price: 0});
-        for (uint256 i = 0; i < _paymentTokens.length; i++) {
+        for (uint256 i = 0; i < paymentTokens.length; i++) {
             IOrderProcessor.PricePoint memory pricePoint =
-                IOrderProcessor(processor).latestFillPrice(token, _paymentTokens[i]);
+                IOrderProcessor(processor).latestFillPrice(token, paymentTokens[i]);
             if (pricePoint.blocktime > latestPricePoint.blocktime) {
                 latestPricePoint = pricePoint;
             }
