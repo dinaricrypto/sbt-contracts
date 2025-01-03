e0ede61918bec084940dc5f43a530f55b2c662ba
jaketimothy <jake.a.timothy@gmail.com>
2025-01-03 09:05:30 -0800

    chore: upgrade script

---
+++ script/nest/upgradedinariadapter.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# op run --env-file="./.env.staging-plume" -- ./script/nest/deploydinariadapter-cmd.sh
+
+op run --env-file="./.env.prod-plume" -- ./script/nest/upgradedinariadapter-cmd.sh
