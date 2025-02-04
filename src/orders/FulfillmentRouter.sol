5c5726c960adfed11f0a60f06388ffaa4cf4a2b1
joshualyguessennd <josue@dinari.com>
2025-02-04 21:45:54 +0000

    import context

--- src/orders/FulfillmentRouter.sol
+++ src/orders/FulfillmentRouter.sol
@@ -2,7 +2,8 @@
 pragma solidity 0.8.25;
 
 import {SafeERC20, IERC20} from "openzeppelin-contracts/contracts/token/ERC20/utils/SafeERC20.sol";
-import {Multicall, Context} from "openzeppelin-contracts/contracts/utils/Multicall.sol";
+import {Multicall} from "openzeppelin-contracts/contracts/utils/Multicall.sol";
+import {Context} from "openzeppelin-contracts/contracts/utils/Context.sol";
 import {ContextUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/utils/ContextUpgradeable.sol";
 import {IVault} from "./IVault.sol";
 import {IOrderProcessor} from "./IOrderProcessor.sol";
